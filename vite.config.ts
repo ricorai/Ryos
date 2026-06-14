d22599c5abadb541dc870512a316d0108a1b929f
ricorai <ricryan2023@gmail.com>
2026-06-15 07:44:14 +0800

    Setup GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,3 @@
-// trigger github pages rebuild
-
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
@@ -7,12 +5,15 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/Ryos/',
+
   plugins: [react(), tailwindcss()],
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, '.'),
     },
   },
+
   server: {
     hmr: process.env.DISABLE_HMR !== 'true',
     watch: process.env.DISABLE_HMR === 'true' ? null : {},
