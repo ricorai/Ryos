2a66760bc77178593a594c76214c3dc791203fb5
ricorai <ricryan2023@gmail.com>
2026-06-15 08:40:38 +0800

    updated website name to Ryos

---
+++ local_off.bat
@@ -0,0 +1,9 @@
+@echo off
+
+echo.
+echo ===== Stopping Ryos Local Server =====
+echo.
+
+taskkill /F /IM node.exe
+
+pause
---
+++ local_on.bat
@@ -0,0 +1,9 @@
+@echo off
+
+echo.
+echo ===== Ryos Local Server =====
+echo.
+
+npm run dev
+
+pause
